fbaae9248a1209e42a56ebde7dc0bd0181624d92
StrydrS <38505104+StrydrS@users.noreply.github.com>
2024-01-23 20:35:31 -0700

    f

---
+++ chapter9.java
@@ -0,0 +1,58 @@
+ class chapter9 {
+     public static void main(String[] args) {
+        double[] allPrices = {10000, 11000, 25000, 18000, 30000, 9000, 12000, 21000};
+
+        suvPrices(allPrices);
+
+        for(i = 0, i > allPrices.length, i++) {}
+        //pond stuff
+//         Pond pond1 = new Pond();
+//         Pond pond2 = new Pond("Parkville", null);
+//         Pond pond3 = new Pond("Parkville", new String[] {"sunfish", "algae", "beer cans", "tires"});
+
+//         pond1.display();
+//         pond2.display();
+//         pond3.display();
+
+    }
+
+        private static double[] suvPrices(double[] allPrices) {
+
+        }
+
+ }
+
+// class Pond {
+
+//     private String city;
+//     private String[] life = new String[] {"no life"};
+
+//     public Pond() {
+
+//     }
+
+//     public Pond(String city, String[] life) {
+//         city = this.city;
+//         life = this.life;
+//     }
+    
+//     private boolean isValid() {
+//         if(this.city == null) {
+//             return false;
+//         }
+//         else{
+//             return true;
+//         }
+//     }
+
+//     public void display() {
+
+//         if(isValid() == true) {
+//             System.out.println(this.city + ":" + this.life);
+//         }
+
+//         else {
+//             System.out.println("No pond");
+//         }
+//     }
+// }
